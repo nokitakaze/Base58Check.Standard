69725602ec04a09f619b3f68a5e13106adb56b7b
Nokita Kaze <admin@kanaria.ru>
2020-08-09 22:51:35 +0300

    * Rewritten from scratch

--- Base58Check/Base58CheckEncoding.cs
+++ Base58Check/Base58CheckEncoding.cs
@@ -1,141 +1,159 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
 using System.Security.Cryptography;
 
 namespace NokitaKaze.Base58Check
 {
-  /// <summary>
-  /// Base58Check Encoding / Decoding (Bitcoin-style)
-  /// </summary>
-  /// <remarks>
-  /// See here for more details: https://en.bitcoin.it/wiki/Base58Check_encoding
-  /// </remarks>
-  public static class Base58CheckEncoding
-  {
-    private const int CHECK_SUM_SIZE = 4;
-    private const string DIGITS = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
-
     /// <summary>
-    /// Encodes data with a 4-byte checksum
+    /// Base58Check Encoding / Decoding (Bitcoin-style)
     /// </summary>
-    /// <param name="data">Data to be encoded</param>
-    /// <returns></returns>
-    public static string Encode(byte[] data)
+    /// <remarks>
+    /// See here for more details: https://en.bitcoin.it/wiki/Base58Check_encoding
+    /// </remarks>
+    public static class Base58CheckEncoding
     {
-      return EncodePlain(_AddCheckSum(data));
-    }
+        public const int CHECK_SUM_SIZE = 4;
 
-    /// <summary>
-    /// Encodes data in plain Base58, without any checksum.
-    /// </summary>
-    /// <param name="data">The data to be encoded</param>
-    /// <returns></returns>
-    public static string EncodePlain(byte[] data)
-    {
-      // Decode byte[] to BigInteger
-      var intData = data.Aggregate<byte, BigInteger>(0, (current, t) => current*256 + t);
+        /// <summary>
+        /// "Don't want 0OIl characters that look the same in some fonts and"
+        /// </summary>
+        public const string ALPHABET = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
 
-      // Encode BigInteger to Base58 string
-      var result = string.Empty;
-      while (intData > 0)
-      {
-        var remainder = (int)(intData % 58);
-        intData /= 58;
-        result = DIGITS[remainder] + result;
-      }
+        public static readonly BigInteger Base58BI = new BigInteger(58);
+        private static readonly BigInteger[] Numbers;
 
-      // Append `1` for each leading 0 byte
-      for (var i = 0; i < data.Length && data[i] == 0; i++)
-      {
-        result = '1' + result;
-      }
-
-      return result;
-    }
-
-    /// <summary>
-    /// Decodes data in Base58Check format (with 4 byte checksum)
-    /// </summary>
-    /// <param name="data">Data to be decoded</param>
-    /// <returns>Returns decoded data if valid; throws FormatException if invalid</returns>
-    public static byte[] Decode(string data)
-    {
-      var dataWithCheckSum = DecodePlain(data);
-      var dataWithoutCheckSum = _VerifyAndRemoveCheckSum(dataWithCheckSum);
-
-      if (dataWithoutCheckSum == null)
-      {
-        throw new FormatException("Base58 checksum is invalid");
-      }
-
-      return dataWithoutCheckSum;
-    }
-
-    /// <summary>
-    /// Decodes data in plain Base58, without any checksum.
-    /// </summary>
-    /// <param name="data">Data to be decoded</param>
-    /// <returns>Returns decoded data if valid; throws FormatException if invalid</returns>
-    public static byte[] DecodePlain(string data)
-    {
-      // Decode Base58 string to BigInteger 
-      BigInteger intData = 0;
-      for (var i = 0; i < data.Length; i++)
-      {
-        var digit = DIGITS.IndexOf(data[i]); //Slow
-
-        if (digit < 0)
+        static Base58CheckEncoding()
         {
-          throw new FormatException(string.Format("Invalid Base58 character `{0}` at position {1}", data[i], i));
+            Numbers = Enumerable
+                .Range(0, 58)
+                .Select(t => new BigInteger(t))
+                .ToArray();
         }
 
-        intData = intData * 58 + digit;
-      }
+        /// <summary>
+        /// Encodes data in plain Base58, without any checksum
+        /// </summary>
+        /// <param name="input">The data to be encoded</param>
+        /// <returns></returns>
+        public static string EncodePlain(ICollection<byte> input)
+        {
+            BigInteger inputInteger;
+            {
+                inputInteger = BigInteger.Zero;
+                var offset = BigInteger.One;
+                var num256 = new BigInteger(256);
 
-      // Encode BigInteger to byte[]
-      // Leading zero bytes get encoded as leading `1` characters
-      var leadingZeroCount = data.TakeWhile(c => c == '1').Count();
-      var leadingZeros = Enumerable.Repeat((byte)0, leadingZeroCount);
-      var bytesWithoutLeadingZeros =
-        intData.ToByteArray()
-        .Reverse()// to big endian
-        .SkipWhile(b => b == 0);//strip sign byte
-      var result = leadingZeros.Concat(bytesWithoutLeadingZeros).ToArray();
+                foreach (var t in input.Reverse().ToArray())
+                {
+                    inputInteger += t * offset;
+                    offset *= num256;
+                }
+            }
 
-      return result;
+            var result = "";
+            while (inputInteger > 0)
+            {
+                var charOffset = (int) (inputInteger % Base58BI);
+                result = ALPHABET[charOffset] + result;
+                inputInteger /= Base58BI;
+            }
+
+            int prefixZeroCount = input
+                .TakeWhile(t => t == 0)
+                .Count();
+
+            return "".PadLeft(prefixZeroCount, '1') + result;
+        }
+
+        /// <summary>
+        /// Decodes data in plain Base58, without any checksum.
+        /// </summary>
+        /// <param name="data">Data to be decoded</param>
+        /// <returns>Returns decoded data if valid; throws FormatException if invalid</returns>
+        public static byte[] DecodePlain(string data)
+        {
+            BigInteger result;
+            {
+                result = BigInteger.Zero;
+                var offset = BigInteger.One;
+
+                var reversedBytes = Enumerable
+                    .Range(0, data.Length)
+                    .Select(t => data.Substring(t, 1))
+                    .Reverse()
+                    .ToArray();
+
+                foreach (var c in reversedBytes)
+                {
+                    var digit = ALPHABET.IndexOf(c, StringComparison.Ordinal);
+                    if (digit == -1)
+                    {
+                        throw new FormatException(string.Format("Invalid Base58 character `{0}`", c));
+                    }
+
+                    result += Numbers[digit] * offset;
+                    offset *= Base58BI;
+                }
+            }
+
+            var prefixZeroCount = data
+                .TakeWhile(c => c == '1')
+                .Count();
+
+            var withoutPrefix = result
+                .ToByteArray()
+                .Reverse()
+                .SkipWhile(t => t == 0)
+                .ToArray();
+
+            return Enumerable
+                .Repeat((byte) 0, prefixZeroCount)
+                .Concat(withoutPrefix)
+                .ToArray();
+        }
+
+        /// <summary>
+        /// Encodes data with a 4-byte checksum
+        /// </summary>
+        /// <param name="data">Data to be encoded</param>
+        /// <returns></returns>
+        public static string Encode(ICollection<byte> data)
+        {
+            var checksum = GetCheckSum(data);
+
+            return EncodePlain(data.Concat(checksum).ToArray());
+        }
+
+        /// <summary>
+        /// Decodes data in Base58Check format (with 4 byte checksum)
+        /// </summary>
+        /// <param name="data">Data to be decoded</param>
+        /// <returns>Returns decoded data if valid; throws FormatException if invalid</returns>
+        public static byte[] Decode(string data)
+        {
+            var decoded = DecodePlain(data);
+            var dataItself = decoded.Take(decoded.Length - CHECK_SUM_SIZE).ToArray();
+            var checksum = decoded.Skip(decoded.Length - CHECK_SUM_SIZE).ToArray();
+
+            var realChecksum = GetCheckSum(dataItself);
+
+            if (!realChecksum.SequenceEqual(checksum))
+            {
+                throw new FormatException("Base58 checksum is invalid");
+            }
+
+            return dataItself;
+        }
+
+        public static byte[] GetCheckSum(ICollection<byte> input)
+        {
+            using var sha256 = SHA256.Create();
+            var hash1 = sha256.ComputeHash(input.ToArray());
+            var hash2 = sha256.ComputeHash(hash1);
+
+            return hash2.Take(CHECK_SUM_SIZE).ToArray();
+        }
     }
-
-    private static byte[] _AddCheckSum(byte[] data)
-    {
-      var checkSum = _GetCheckSum(data);
-      var dataWithCheckSum = ArrayHelpers.ConcatArrays(data, checkSum);
-
-      return dataWithCheckSum;
-    }
-
-    //Returns null if the checksum is invalid
-    private static byte[] _VerifyAndRemoveCheckSum(byte[] data)
-    {
-      var result = ArrayHelpers.SubArray(data, 0, data.Length - CHECK_SUM_SIZE);
-      var givenCheckSum = ArrayHelpers.SubArray(data, data.Length - CHECK_SUM_SIZE);
-      var correctCheckSum = _GetCheckSum(result);
-
-      return givenCheckSum.SequenceEqual(correctCheckSum) ? result : null;
-    }
-
-    private static byte[] _GetCheckSum(byte[] data)
-    {
-      using (var sha256 = SHA256.Create())
-      {
-        var hash1 = sha256.ComputeHash(data);
-        var hash2 = sha256.ComputeHash(hash1);
-
-        var result = new byte[CHECK_SUM_SIZE];
-        Buffer.BlockCopy(hash2, 0, result, 0, result.Length);
-
-        return result;
-      }     
-    }
-  }
 }
